6a2761d2f164e2aee6b17543cb5f4b0302ac5350
Romain HENRY <romainhenry60@gmail.com>
2022-02-13 12:57:30 +0100

    snake - add movement

--- snake/classes/game.py
+++ snake/classes/game.py
@@ -6,23 +6,32 @@ from .snake import Snake
 class Game:
     def __init__(self):
         self.screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
-        self.screen.fill(WHITE)
         pygame.display.set_caption("SNAKE")
-        self.snake_group = pygame.sprite.Group()
+        self.group = pygame.sprite.Group()
         self.snake = Snake()
+        self.clock = pygame.time.Clock()
 
     def run(self):
 
         running = True
+        self.group.add(self.snake)
         while running:
-
-            self.snake_group.add(self.snake)
-            self.update()
+            self.clock.tick(60)
+            self.screen.fill(WHITE)
+            self.group.draw(self.screen)
+            self.group.update()
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT and self.snake.direction != 'RIGHT':
+                        self.snake.direction = 'LEFT'
+                    elif event.key == pygame.K_RIGHT and self.snake.direction != 'LEFT':
+                        self.snake.direction = 'RIGHT'
+                    elif event.key == pygame.K_UP and self.snake.direction != 'DOWN':
+                        self.snake.direction = 'UP'
+                    elif event.key == pygame.K_DOWN and self.snake.direction != 'UP':
+                        self.snake.direction = 'DOWN'
 
-    def update(self):
-        self.snake_group.draw(self.screen)
-        pygame.display.update()
+            pygame.display.update()
--- snake/classes/snake.py
+++ snake/classes/snake.py
@@ -6,6 +6,27 @@ class Snake(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load("./img/TETE.png").convert_alpha()
+        self.head = self.image
         self.rect = self.image.get_rect()
         self.rect.x = WIN_WIDTH / 2
         self.rect.y = WIN_HEIGHT / 2
+        self.speed = 4
+        self.direction = "LEFT"
+        self.group = pygame.sprite.Group()
+
+    def update(self):
+        current_x = self.rect.x
+        current_y = self.rect.y
+
+        if self.direction == 'LEFT':
+            self.image = pygame.transform.rotate(self.head, 90)
+            self.rect.x -= self.speed
+        elif self.direction == 'RIGHT':
+            self.image = pygame.transform.rotate(self.head, -90)
+            self.rect.x += self.speed
+        elif self.direction == 'UP':
+            self.image = pygame.transform.rotate(self.head, 0)
+            self.rect.y -= self.speed
+        elif self.direction == 'DOWN':
+            self.image = pygame.transform.rotate(self.head, 180)
+            self.rect.y += self.speed
